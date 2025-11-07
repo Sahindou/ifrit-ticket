b9132fbbb6b743f97a65048f37f482cd07e88338
Sahindou Abdallah <sahindouabdallah@gmail.com>
2025-11-07 02:56:09 +0100

    feat&fix(client): mise en place des types restant & modification mineur sur le typage

--- apps/client/src/types/ticket.ts
+++ apps/client/src/types/ticket.ts
@@ -1,14 +1,22 @@
-export type TicketType = 'bug' | 'feature' | 'update' | 'hotfix';
-export type TicketPriority = 'low' | 'medium' | 'high' | 'urgent';
-export type TicketStatus = 'todo' | 'in_progress' | 'done';
+export type TicketPriority = 'LOW' | 'MEDIUM' | 'HIGH';
+export type TicketStatus = 'TO_DO' | 'IN_PROGRESS' | 'DONE';
 
 export interface Ticket {
   id: string;
   title: string;
   description: string;
-  type: TicketType;
   priority: TicketPriority;
   status: TicketStatus;
   due_date: string;
+  type_id: string | null;
   created_at: string;
+}
+
+export interface TicketCreateOrUpdate {
+  title: string;
+  description: string;
+  priority: TicketPriority;
+  status: TicketStatus;
+  due_date: string;
+  type_id: string | null;
 }
